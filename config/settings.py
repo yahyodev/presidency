0d22033c3a6382693866bc869837d78344c66ab3
Yahyobek Abdunazarov <y.abdunazarov@uicgroup.org>
2022-09-17 19:43:49 +0500

    feat

--- config/settings.py
+++ config/settings.py
@@ -156,7 +156,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 #     BASE_DIR / 'static',
 # )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
