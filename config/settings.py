b5f215fdfdf48a62decf069b85f3c0e76b8957a1
Yahyobek Abdunazarov <y.abdunazarov@uicgroup.org>
2022-09-17 19:47:45 +0500

    feat

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic", #third party
     'django.contrib.staticfiles',
     # custom apps
     'core',
@@ -156,7 +157,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 #     BASE_DIR / 'static',
 # )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
